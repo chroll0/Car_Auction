f5d0d6bb542c2023db0da230742e196c7d830f9f
Saba Antadze <sabaantadze@gmail.com>
2024-09-26 16:22:47 +0400

    fix: make car gallery component work

--- app/pages/auction_details/page.tsx
+++ app/pages/auction_details/page.tsx
@@ -6,11 +6,14 @@ import {
 
 const AuctionDetails = () => {
   return (
-    <div className="flex flex-col paddingX paddingY gap-8 justify-center">
-      <div className="flex flex-col">
-        <h1>1986 Porsche 928 S</h1>
-        <p>5.0-Liter V8 Power, Pop-Up Headlights, Extensive Modifications</p>
+    <div className="flex flex-col paddingX paddingY gap-6 justify-center">
+      <div className="flex flex-col gap-1 capitalize text-black-90 font-semibold ">
+        <h1 className="text-[24px] leading-[36px]">1986 Porsche 928 S</h1>
+        <p className="text-[16px] leading-[24px] ">
+          5.0-Liter V8 Power, Pop-Up Headlights, Extensive Modifications
+        </p>
       </div>
+
       <Gallery />
       <div className="flex 2xl:flex-row flex-col gap-10 py-4">
         <BidInformation />
--- components/auction_details/gallery.tsx
+++ components/auction_details/gallery.tsx
@@ -1,75 +1,53 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
-import React from "react";
-import { Button } from "../reusable";
 
 const Gallery = () => {
+  const gallery = [
+    "/img/car1.png",
+    "/img/car2.png",
+    "/img/car3.png",
+    "/img/car4.png",
+  ];
+
+  const [mainImage, setMainImage] = useState(gallery[0]);
+
+  // Function to handle thumbnail click
+  const handleThumbnailClick = (clickedImage: string) => {
+    setMainImage(clickedImage);
+  };
+
   return (
     <div className="flex flex-col gap-6 w-full">
       <div className="w-full">
-        <Image
-          src="/img/car1.png"
-          alt="car"
-          width={3000}
-          height={2000}
-          style={{ width: "auto", height: "auto" }}
-        />
-        <div className="flex pt-4 gap-8 overflow-x-auto">
+        {/* Main large image */}
+        <div className="w-[full]">
           <Image
-            src="/img/car1.png"
-            alt="car"
-            width={200}
-            height={200}
-            style={{ width: "auto", height: "auto" }}
+            src={mainImage}
+            alt="main car"
+            width={3000}
+            height={2000}
+            style={{ width: "full", height: "full" }}
           />
-          <Image
-            src="/img/car1.png"
-            alt="car"
-            width={200}
-            height={200}
-            style={{ width: "auto", height: "auto" }}
-          />
-          <Image
-            src="/img/car1.png"
-            alt="car"
-            width={200}
-            height={200}
-            style={{ width: "auto", height: "auto" }}
-          />
-          <Image
-            src="/img/car1.png"
-            alt="car"
-            width={200}
-            height={200}
-            style={{ width: "auto", height: "auto" }}
-          />
-          <Image
-            src="/img/car1.png"
-            alt="car"
-            width={200}
-            height={200}
-            style={{ width: "auto", height: "auto" }}
-          />
-          <Image
-            src="/img/car1.png"
-            alt="car"
-            width={200}
-            height={200}
-            style={{ width: "auto", height: "auto" }}
-          />
-          <Image
-            src="/img/car1.png"
-            alt="car"
-            width={200}
-            height={200}
-            style={{ width: "auto", height: "auto" }}
-          />
-          <Image
-            src="/img/car1.png"
-            alt="car"
-            width={200}
-            height={200}
-            style={{ width: "auto", height: "auto" }}
-          />
+        </div>
+        <div className="flex pt-4 gap-6 overflow-x-auto">
+          {/* Thumbnails */}
+          {gallery.map((element, index) => (
+            <div
+              key={index}
+              className="w-[200px] h-[110px] rounded-[10px] transition-all cursor-pointer border-[2.5px] border-white hover:border-blue-10"
+              onClick={() => handleThumbnailClick(element)}
+            >
+              <Image
+                src={element}
+                alt={`car ${index + 1}`}
+                width={200}
+                height={200}
+                className="rounded-[10px]"
+              />
+            </div>
+          ))}
         </div>
       </div>
     </div>
